164904d2a06e6a4835d525187cff4dbf19797310
leader228228 <leader228228@gmail.com>
2019-02-08 23:21:04 +0200

    leader's second commit

--- Dodo/Form1.Designer.cs
+++ Dodo/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // checkedListBox1
@@ -39,14 +41,23 @@
             this.checkedListBox1.Size = new System.Drawing.Size(196, 89);
             this.checkedListBox1.TabIndex = 0;
             // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(106, 91);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.checkedListBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -54,6 +65,7 @@
         #endregion
 
         private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
 
