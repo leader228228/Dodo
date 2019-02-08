75f0342ca000224963c41a5fd03d550236594c2c
Marina <marylostherself@gmail.com>
2019-02-08 23:22:51 +0200

    fcgfcgfhcgjvjg

--- Dodo/Form1.Designer.cs
+++ Dodo/Form1.Designer.cs
@@ -28,58 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(196, 181);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(196, 89);
+            this.checkedListBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(95, 32);
+            this.button1.Location = new System.Drawing.Point(612, 53);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(45, 368);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "New";
+            this.button1.Size = new System.Drawing.Size(75, 321);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // checkedListBox2
             // 
-            this.button2.Location = new System.Drawing.Point(146, 205);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(211, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Edit";
-            this.button2.UseVisualStyleBackColor = true;
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Location = new System.Drawing.Point(110, 276);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(252, 89);
+            this.checkedListBox2.TabIndex = 2;
             // 
-            // button3
+            // label1
             // 
-            this.button3.Location = new System.Drawing.Point(312, 234);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(45, 166);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Dodo/Form1.cs
+++ Dodo/Form1.cs
@@ -21,5 +21,10 @@ namespace Dodo
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
